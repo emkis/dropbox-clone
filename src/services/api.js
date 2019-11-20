b7e342aa1059444e1773e998f00569aea7e713f1
Nicolas Jardim <nicolasemkis@gmail.com>
2019-11-20 01:25:09 -0300

    .env file created and changed .env name

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3333',
 })
 
 export default api
